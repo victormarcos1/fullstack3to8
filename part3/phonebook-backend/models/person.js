f8ddde8067d6b9bee251a0619383a0b9cca521cd
victormarcos1 <129120518+victormarcos1@users.noreply.github.com>
2023-11-16 15:54:20 -0300

    Add files via upload

---
+++ part3/phonebook-backend/models/person.js
@@ -0,0 +1,47 @@
+require('dotenv').config({ path: '../.env' })
+const mongoose = require('mongoose')
+const uniqVal = require('mongoose-unique-validator')
+
+// eslint-disable-next-line
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose
+  .connect(url)
+// eslint-disable-next-line no-unused-vars
+  .then((result) => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    index: true,
+    unique: true,
+    minlength: 3,
+  },
+  number: {
+    type: String,
+    required: true,
+    index: true,
+    unique: true,
+    minlength: 8,
+  },
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+personSchema.plugin(uniqVal)
+
+module.exports = mongoose.model('Person', personSchema)
